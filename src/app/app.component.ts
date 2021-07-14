646785cae20e369614abc2bc0dc1bcc1ed8f1fde
nguyenduynamhg <nam080802123108@gmail.com>
2021-07-14 18:31:49 +0700

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+  hoa = {
+    name: 'Nam',
+    age: 22
+  };
+  handle() {
+    alert('Hello');
+  }
 }
